44af5244040d773380782cd8c4bfd5a1b30ec787
pang912 <1224269206@qq.com>
2022-03-05 13:15:43 +0800

    删除dev分支文件

--- 50dayDemo/hidden-search-widget/script.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementsByClassName('btn')[0];
-var search = document.getElementsByClassName('search')[0];
-var input = document.getElementsByClassName('input')[0];
-
-btn.addEventListener('click', () => {
-    search.classList.toggle('active');
-    input.focus();
-})
